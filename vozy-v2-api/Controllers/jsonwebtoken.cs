5fea28adf99c31f3edf25f3cfff9db6a00a8aa9d
Diego Jared  Donis Soto <programacion4@recagua.com>
2022-04-07 13:34:02 -0600

    version para pruebas

--- vozy-v2-api/Controllers/jsonwebtoken.cs
+++ vozy-v2-api/Controllers/jsonwebtoken.cs
@@ -2,38 +2,38 @@
 using JWT.Builder;
 namespace vozy_v2_api.Controllers
 {
-    public class jsonwebtoken
+  public class jsonwebtoken
+  {
+    private readonly string secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
+    public string crearToken(string usuario, string password)
     {
-        private readonly string secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
-        public string crearToken(string usuario, string password)
-        {
-            var token = JwtBuilder.Create()
-                      .WithAlgorithm(new HMACSHA256Algorithm()) // symmetric
-                      .WithSecret(secret)
-                      .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(24).ToUnixTimeSeconds())
-                      .AddClaim("user", usuario)
-                      .AddClaim("password", password)
-                      .Encode();
+      var token = JwtBuilder.Create()
+                .WithAlgorithm(new HMACSHA256Algorithm()) // symmetric
+                .WithSecret(secret)
+                .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(24).ToUnixTimeSeconds())
+                .AddClaim("user", usuario)
+                .AddClaim("password", password)
+                .Encode();
 
-            return token;
-        }
-
-        public bool validarToken(string token)
-        {
-            try
-            {
-                JwtBuilder.Create()
-                                .WithAlgorithm(new HMACSHA256Algorithm()) // symmetric
-                                .WithSecret(secret)
-                                .MustVerifySignature()
-                                .Decode(token);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-
-        }
+      return token;
     }
+
+    public bool validarToken(string token)
+    {
+      try
+      {
+        JwtBuilder.Create()
+                        .WithAlgorithm(new HMACSHA256Algorithm()) // symmetric
+                        .WithSecret(secret)
+                        .MustVerifySignature()
+                        .Decode(token);
+        return true;
+      }
+      catch
+      {
+        return false;
+      }
+
+    }
+  }
 }
